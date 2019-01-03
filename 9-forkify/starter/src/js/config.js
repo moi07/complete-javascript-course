5b6cc0e5b70b031bf8ead80306771672638912f5
moi07 <mjordash@gmail.com>
2019-01-03 18:32:09 +0200

    Section 9 - up to lecture 145(API call limit reached)

---
+++ 9-forkify/starter/src/js/config.js
@@ -0,0 +1,2 @@
+export const key = "12a6803f43ba240903c49ad16b386a7b";
+export const proxy = "https://cors-anywhere.herokuapp.com/";
--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,4 +1,58 @@
-// Global app controller
-import num from "./test";
-const x = 23;
-console.log(`${num + 54} and ${x}`);
+import Search from "./models/Search";
+import Recipe from "./models/Recipe";
+import * as searchView from "./views/searchView";
+import { elements, renderLoader, clearLoader } from "./views/base";
+
+/** Global state of the app
+ * - Search object
+ * - Current recipe object
+ * - Shopping lst object
+ * - Liked recipes
+ */
+const state = {};
+
+/**
+ * SEARCH Controller
+ */
+const controlSearch = async () => {
+  //Get query from view
+  const query = searchView.getInput(); //TODO
+  console.log(query);
+
+  if (query) {
+    //New Search object and add to state
+    state.search = new Search(query);
+    // Prepare UI for results
+    searchView.clearInput();
+    searchView.clearResults();
+    renderLoader(elements.searchRes);
+
+    //Search for recipes
+    await state.search.getResults();
+
+    //Render results on UI
+    clearLoader();
+    searchView.renderResults(state.search.result);
+  }
+};
+
+elements.searchForm.addEventListener("submit", e => {
+  e.preventDefault();
+  controlSearch();
+});
+
+elements.searchResPages.addEventListener("click", e => {
+  const btn = e.target.closest(".btn-inline");
+  if (btn) {
+    const goToPage = parseInt(btn.dataset.goto, 10);
+    searchView.clearResults();
+    searchView.renderResults(state.search.result, goToPage);
+  }
+});
+
+/**
+ * RECIPE Controller
+ */
+
+const r = new Recipe(46956);
+r.getRecipe();
